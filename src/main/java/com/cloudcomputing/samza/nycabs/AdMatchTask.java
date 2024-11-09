6beccf1edb6fbbf3c5f75399044920831ad7575d
kevin0988459 <kevin0988459@gmail.com>
2024-11-09 07:35:50 -0500

    a

--- src/main/java/com/cloudcomputing/samza/nycabs/AdMatchTask.java
+++ src/main/java/com/cloudcomputing/samza/nycabs/AdMatchTask.java
@@ -1,7 +1,14 @@
 package com.cloudcomputing.samza.nycabs;
 
-import com.google.common.io.Resources;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.samza.context.Context;
+import org.apache.samza.storage.kv.KeyValueIterator;
 import org.apache.samza.storage.kv.KeyValueStore;
 import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.system.OutgoingMessageEnvelope;
@@ -9,29 +16,20 @@ import org.apache.samza.task.InitableTask;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
 import org.apache.samza.task.TaskCoordinator;
-
 import org.codehaus.jackson.map.ObjectMapper;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.HashMap;
-import java.util.Arrays;
-
+import com.google.common.io.Resources;
 
 /**
- * Consumes the stream of events.
- * Outputs a stream which handles static file and one stream
- * and gives a stream of advertisement matches.
+ * Consumes the stream of events. Outputs a stream which handles static file and
+ * one stream and gives a stream of advertisement matches.
  */
 public class AdMatchTask implements StreamTask, InitableTask {
 
     /*
        Define per task state here. (kv stores etc)
        READ Samza API part in Writeup to understand how to start
-    */
-
+     */
     private KeyValueStore<Integer, Map<String, Object>> userInfo;
 
     private KeyValueStore<String, Map<String, Object>> yelpInfo;
@@ -94,8 +92,8 @@ public class AdMatchTask implements StreamTask, InitableTask {
     }
 
     /**
-     * This function will read the static data from resources folder
-     * and save data in KV store.
+     * This function will read the static data from resources folder and save
+     * data in KV store.
      * <p>
      * This is just an example, feel free to change them.
      */
@@ -144,14 +142,99 @@ public class AdMatchTask implements StreamTask, InitableTask {
         sharing the same blockId will arrive at the same task, similar to the
         approach that MapReduce sends all the key value pairs with the same key
         into the same reducer.
-        */
+         */
         String incomingStream = envelope.getSystemStreamPartition().getStream();
+        Map<String, Object> message = (Map<String, Object>) envelope.getMessage();
+        int userId = (Integer) message.get("userId");
 
         if (incomingStream.equals(AdMatchConfig.EVENT_STREAM.getStream())) {
             // Handle Event messages
+            String eventType = (String) message.get("type");
+
+            switch (eventType) {
+                case "RIDER_STATUS":
+                    processRiderStatus(userId, message);
+                    break;
+                case "RIDER_INTEREST":
+                    processRiderInterest(userId, message);
+                    break;
+                case "RIDE_REQUEST":
+                    processRideRequest(userId, collector);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected event type: " + eventType);
+            }
 
         } else {
             throw new IllegalStateException("Unexpected input stream: " + envelope.getSystemStreamPartition());
         }
+    }
+
+    private void processRiderStatus(int userId, Map<String, Object> message) {
+        // Update user's mood, blood_sugar, stress, active in userInfo store
+        Map<String, Object> user = userInfo.get(userId);
+        if (user != null) {
+            user.put("mood", message.get("mood"));
+            user.put("blood_sugar", message.get("blood_sugar"));
+            user.put("stress", message.get("stress"));
+            user.put("active", message.get("active"));
+            userInfo.put(userId, user);
+        }
+    }
+
+    private void processRiderInterest(int userId, Map<String, Object> message) {
+        // Update user's interest if duration > 5 minutes
+        int duration = (Integer) message.get("duration");
+        if (duration > 300000) { // 5 minutes in milliseconds
+            Map<String, Object> user = userInfo.get(userId);
+            if (user != null) {
+                user.put("interest", message.get("interest"));
+                userInfo.put(userId, user);
+            }
+        }
+    }
+
+    private void processRideRequest(int userId, MessageCollector collector) {
+        // Calculate ad-match score and send to ad-stream
+        Map<String, Object> user = userInfo.get(userId);
+        if (user != null) {
+            String bestStoreId = null;
+            String bestStoreName = null;
+            double highestScore = 0;
+
+            // Use a KeyValueIterator to iterate over the yelpInfo store
+            KeyValueIterator<String, Map<String, Object>> iterator = yelpInfo.all();
+            try {
+                while (iterator.hasNext()) {
+                    org.apache.samza.storage.kv.Entry<String, Map<String, Object>> entry = iterator.next();
+                    String storeId = entry.getKey();
+                    Map<String, Object> store = entry.getValue();
+                    double score = calculateMatchScore(user, store);
+
+                    if (score > highestScore) {
+                        highestScore = score;
+                        bestStoreId = storeId;
+                        bestStoreName = (String) store.get("name");
+                    }
+                }
+            } finally {
+                iterator.close(); // Ensure the iterator is closed to release resources
+            }
+
+            if (bestStoreId != null) {
+                Map<String, Object> adMessage = new HashMap<>();
+                adMessage.put("userId", userId);
+                adMessage.put("storeId", bestStoreId);
+                adMessage.put("name", bestStoreName);
+                collector.send(new OutgoingMessageEnvelope(AdMatchConfig.AD_STREAM, adMessage));
+            }
+        }
+    }
+
+    private double calculateMatchScore(Map<String, Object> user, Map<String, Object> store) {
+        // Implement the logic to calculate the match score based on the provided criteria
+        double score = (Integer) store.get("review_count") * (Double) store.get("rating");
+        // Add additional logic for interest, device, and distance adjustments
+        return score;
     }
 }
--- src/main/java/com/cloudcomputing/samza/nycabs/application/AdMatchTaskApplication.java
+++ src/main/java/com/cloudcomputing/samza/nycabs/application/AdMatchTaskApplication.java
@@ -1,20 +1,22 @@
 package com.cloudcomputing.samza.nycabs.application;
 
-import com.cloudcomputing.samza.nycabs.AdMatchTask;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import org.apache.samza.application.TaskApplication;
-import org.apache.samza.application.descriptors.TaskApplicationDescriptor;
-import org.apache.samza.serializers.JsonSerde;
-import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
-import org.apache.samza.system.kafka.descriptors.KafkaInputDescriptor;
-import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
-import org.apache.samza.task.StreamTaskFactory;
-
 import java.util.List;
 import java.util.Map;
 
+import org.apache.samza.application.TaskApplication;
+import org.apache.samza.application.descriptors.TaskApplicationDescriptor;
+import org.apache.samza.serializers.JsonSerde;
+import org.apache.samza.system.kafka.descriptors.KafkaInputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaOutputDescriptor;
+import org.apache.samza.system.kafka.descriptors.KafkaSystemDescriptor;
+import org.apache.samza.task.StreamTaskFactory;
+
+import com.cloudcomputing.samza.nycabs.AdMatchTask;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
 public class AdMatchTaskApplication implements TaskApplication {
+
     // Consider modify this zookeeper address, localhost may not be a good choice.
     // If this task application is executing in slave machine.
     private static final List<String> KAFKA_CONSUMER_ZK_CONNECT = ImmutableList.of("localhost:2181");
@@ -26,21 +28,18 @@ public class AdMatchTaskApplication implements TaskApplication {
     @Override
     public void describe(TaskApplicationDescriptor taskApplicationDescriptor) {
         // Define a system descriptor for Kafka.
-        KafkaSystemDescriptor kafkaSystemDescriptor =
-                new KafkaSystemDescriptor("kafka").withConsumerZkConnect(KAFKA_CONSUMER_ZK_CONNECT)
+        KafkaSystemDescriptor kafkaSystemDescriptor
+                = new KafkaSystemDescriptor("kafka").withConsumerZkConnect(KAFKA_CONSUMER_ZK_CONNECT)
                         .withProducerBootstrapServers(KAFKA_PRODUCER_BOOTSTRAP_SERVERS)
                         .withDefaultStreamConfigs(KAFKA_DEFAULT_STREAM_CONFIGS);
 
-        // Hint about streams, please refer to AdMatchConfig.java
-        // We need one input stream "events", one output stream "ad-stream".
+        KafkaInputDescriptor<Map<String, Object>> inputDescriptor
+                = kafkaSystemDescriptor.getInputDescriptor("events", new JsonSerde<>());
+        KafkaOutputDescriptor<Map<String, Object>> outputDescriptor
+                = kafkaSystemDescriptor.getOutputDescriptor("ad-stream", new JsonSerde<>());
 
-        // Define your input and output descriptor in here.
-        // Reference solution:
-        //  https://github.com/apache/samza-hello-samza/blob/master/src/main/java/samza/examples/wikipedia/task/application/WikipediaStatsTaskApplication.java
-
-        // Bound you descriptor with your taskApplicationDescriptor in here.
-        // Please refer to the same link.
-
-        taskApplicationDescriptor.withTaskFactory((StreamTaskFactory)() -> new AdMatchTask());
+        taskApplicationDescriptor.withInputStream(inputDescriptor);
+        taskApplicationDescriptor.withOutputStream(outputDescriptor);
+        taskApplicationDescriptor.withTaskFactory((StreamTaskFactory) () -> new AdMatchTask());
     }
 }
